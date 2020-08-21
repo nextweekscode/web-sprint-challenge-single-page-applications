5ec58ddc7a85a98569c4beb1fb0aeddbb9ac7c2a
Ashton <ashtoturn@gmail.com>
2020-08-21 11:57:38 -0500

    cannot get cypress working

---
+++ src/Confirmation.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+
+
+export default function Confirmation({ information }) {
+  return (
+    
+      <>
+        <h3>Order confirmation ! </h3>
+        <p>Customer Name : {information.username}</p>
+        <p>Address:{information.address}</p>
+        <p>Pizza size:{information.size}</p>
+        <p>Special request: {information.special}</p>
+        {!!information.topping && !!information.topping.length && (
+          <div>
+            Toppings:
+            <ul>
+              {information.topping.map((like, idx) => (
+                <li key={idx}>{like}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+        </>
+      
+    
+  );
+}
+
---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+import { Link } from 'react-router-dom'
+
+
+export default function Home() {
+    return (
+    <Home>
+        <Link to='/pizza'><h2> ORDER PIZZA</h2> </Link>
+    </Home>
+    )
+
+}
